5067844a0097319cadc7e74d52c7624325ac4eb3
fouadbouklada <fouadtvdyn@gmail.com>
2024-06-17 16:38:06 +0100

    Update config.db.js

--- database/config.db.js
+++ database/config.db.js
@@ -1,6 +1,6 @@
 const config = {
    //url : 'mongodb://127.0.0.1:27017/myDb'
   // url : 'mongodb+srv://touatiamine972:mongo123@cluster0.5cdwxp3.mongodb.net/'
-  url: 'mongodb+srv://fouadtvdyn:fouad@1991@cluster0.m4vjdgn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  url: 'mongodb+srv://fouadtv:fouad@1991@cluster0.m4vjdgn.mongodb.net/'
 }
 module.exports = config;
