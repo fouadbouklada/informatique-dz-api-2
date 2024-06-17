2201386fa5fae7196231975ba3c25390b8e52ad8
fouadbouklada <fouadtvdyn@gmail.com>
2024-06-17 16:26:26 +0100

    Update config.db.js

--- database/config.db.js
+++ database/config.db.js
@@ -1,6 +1,6 @@
 const config = {
    //url : 'mongodb://127.0.0.1:27017/myDb'
   // url : 'mongodb+srv://touatiamine972:mongo123@cluster0.5cdwxp3.mongodb.net/'
-  url: 'mongodb+srv://fouadtvdyn:fouad@1991@cluster0.m4vjdgn.mongodb.net/'
+  url: 'mongodb+srv://fouadtvdyn:fouad@1991@cluster0.m4vjdgn.mongodb.net/infoDz'
 }
 module.exports = config;
